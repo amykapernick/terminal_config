91300b0c9ace80d2074aea595c6776f0dd8b2fd9
Amy Kapernick <amy@kapers.dev>
2023-03-03 15:34:20 +0800

    updated pacakges

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -46,11 +46,11 @@ fnm install latest
 fnm use latest
 
 # Setup Node Init
-npm set init.author.name "Amy Kapernick"
-npm set init.author.email $email
-npm set init.author.url "https://amyskapers.dev"
-npm set init.license "MIT"
-npm set init.version "1.0.0"
+npm set init-author-name="Amy Kapernick"
+npm set init-author-email=$email
+npm set init-author-url="https://amyskapers.dev"
+npm set init-license="MIT"
+npm set init-version="1.0.0"
 
 # Install and Setup ZSH and theme
 sudo apt install zsh
@@ -115,10 +115,13 @@ brew install gnupg
 brew install dopplerhq/cli/doppler
 brew tap azure/functions
 brew install azure-functions-core-tools@4
-sudo apt-get install gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libatk-bridge2.0-0 libgbm-dev
+brew install rm-improved
+
+
+sudo apt-get install 
 
 ## apt
-sudo apt-get install gawk
+sudo apt-get install gawk gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libatk-bridge2.0-0 libgbm-dev chromium-browser
 
 ## Charm
 brew tap charmbracelet/tap && brew install charmbracelet/tap/gum
